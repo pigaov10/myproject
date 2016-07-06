a26e0fbceb9ded4ac488707836a48437ad1a4bad
root <root@localhost.localdomain>
2016-07-06 15:52:40 -0300

    added assets

---
+++ module/Blog/src/Blog/Controller/CategoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Blog\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class CategoryController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+
+
+}
+
